082454990a5ea1bd9f0fbd5d227bbbcb764ac055
Piotr <piotrekkk23@op.pl>
2019-11-12 11:41:58 +0100

    user can add photo

--- add_flat.php
+++ add_flat.php
@@ -12,17 +12,29 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
     $locality = $_POST['locality'];
     $street = $_POST['street'];
     $students = $_POST['students'];
+    $photo = $_POST['photo'];
+    
+    $path = "user_data/$userid.jpeg";
+    $finalPath = "http://192.168.1.13/".$path;
 
     require_once 'conn.php';
 
-    $sql = "INSERT INTO flat(userId,description, price, surface,room,type,province,locality,street,students) VALUES('$userid','$description','$price','$surface','$room','$type','$province','$locality','$street','$students')";
+    $sql = "INSERT INTO flat(userId,description, price, surface,room,type,province,locality,street,students,photo) VALUES('$userid','$description','$price','$surface','$room','$type','$province','$locality','$street','$students','$finalPath')";
 
     if(mysqli_query($conn, $sql)) {
-        $result["success"] = "1";
-        $result["message"] = "success";
 
-        echo json_encode($result);
-        mysqli_close($conn);
+        if( file_put_contents($path,base64_decode($photo))) {
+            $result["success"] = "1";
+            $result["message"] = "success";
+    
+            echo json_encode($result);
+            mysqli_close($conn);
+
+        }
+
+        
+
+        
 
     }else {
         $result["success"] = "0";

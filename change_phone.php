09318e70176b36a1f36976be17f1c2a59166a047
Piotr <piotrekkk23@op.pl>
2020-01-02 12:40:07 +0100

    account menagement cleaned

--- change_phone.php
+++ change_phone.php
@@ -15,10 +15,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $auth = authorization($userId, $token);
 
     if ($auth === 1) {
-        if ($stmt->execute()) {
-            $result['success'] = "1";
-            echo json_encode($result);
-            mysqli_close($conn);
+
+        if (preg_match('/^[0-9]{9}$/', $phone)) {
+
+            if ($stmt->execute()) {
+                $result['success'] = "1";
+                echo json_encode($result);
+                mysqli_close($conn);
+            } else {
+                $result['success'] = "0";
+                echo json_encode($result);
+                mysqli_close($conn);
+            }
         } else {
             $result['success'] = "0";
             echo json_encode($result);
